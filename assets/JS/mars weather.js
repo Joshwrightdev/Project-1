a085637b936b0e153f7dc6b649b11f21506d6656
Clare Henderson <henderson2350@gmail.com>
2021-03-31 17:55:24 -0400

    added css, edited html, did some formatting, and added two data points for mars weather

--- assets/JS/mars weather.js
+++ assets/JS/mars weather.js
@@ -1,9 +1,11 @@
 // DOM VARIABLES
-
+marsCardText = $("#mars-card-text")
 // FETCHING MARS WEATHER DATA
 
 apiKey = "EgPBPuCRfLxgIGWykQFEfYbnf48yDhmbDXqAuMAW"
 requestUrl = "https://api.nasa.gov/insight_weather/?api_key=EgPBPuCRfLxgIGWykQFEfYbnf48yDhmbDXqAuMAW&feedtype=json&ver=1.0"
+
+currentSeason = ""
 
 function getApi(requestUrl) {
     fetch(requestUrl)
@@ -13,13 +15,26 @@ function getApi(requestUrl) {
 
             .then(function (data) {
                 console.log(data)
-                var array = data[828]
-                console.log(array.Season)
-                var currentSeason=array.Season
+                var seasonArray = data[828]
+                var currentSeason= seasonArray.Season
+
+                var seasonEl = $("<p>")
+                seasonEl.text("Current Season: "+currentSeason)
+                console.log(currentSeason)
+                marsCardText.append(seasonEl)
+
+                console.log(data[832])
+                array832 = data[832]
+                console.log(array832.PRE.av)
+                avAtmosphericPressure= array832.PRE.av
+
+                var pressureEl = $("<p>")
+                pressureEl.text("Atmospheric Pressure: "+ avAtmosphericPressure)
+                marsCardText.append(pressureEl)
+
             })
             
-            var seasonEl = $("<p>")
-            seasonEl.text("Current Season: "+array.Season)
+
 
 
             
