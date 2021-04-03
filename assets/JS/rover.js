8f80e79f8c218eeafdd7fb0c056ed33fcac3a23b
Jenny Dinh <jenny.p.dinh@gmail.com>
2021-04-02 20:44:29 -0500

    js updates

--- assets/JS/rover.js
+++ assets/JS/rover.js
@@ -14,65 +14,8 @@ const carouselItem = $(".carousel-item img");
 const actualItem = $(".carousel-item");
 //flag to show which image we're on
 let state = true;
-fetch(queryURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    $(carouselItem[0]).attr("src", data.latest_photos[1].img_src);
-    let rover = data.latest_photos[1];
-    let landingEl = createRoverText(rover);
-
-    roverInfoEl.append(landingEl);
-
-    fetch(curQueryURL)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-        let landingEl2 = createRoverText(data.photos[0]);
-
-        roverInfoEl.append(landingEl2);
-        landingEl2.attr("style", "display: none");
-
-        $(carouselItem[1]).attr("src", data.photos[0].img_src);
-        $(".carousel-control-next").on("click", function () {
-          console.log("im so done");
-          if (state) {
-            $(actualItem[0]).removeClass("active");
-            $(actualItem[1]).addClass("active");
-            landingEl2.attr("style", "display: block");
-            landingEl.attr("style", "display: none");
-            state = !state;
-          } else {
-            state = !state;
-            $(actualItem[1]).removeClass("active");
-            $(actualItem[0]).addClass("active");
-            landingEl.attr("style", "display: block");
-            landingEl2.attr("style", "display: none");
-          }
-        });
-
-        $(".carousel-control-prev").on("click", function () {
-          if (state) {
-            state = !state;
-            $(actualItem[0]).removeClass("active");
-            $(actualItem[1]).addClass("active");
-            landingEl2.attr("style", "display: block");
-            landingEl.attr("style", "display: none");
-          } else {
-            state = !state;
-            $(actualItem[1]).removeClass("active");
-            $(actualItem[0]).addClass("active");
-            landingEl.attr("style", "display: block");
-            landingEl2.attr("style", "display: none");
-          }
-        });
-      });
-  });
-
+​
+​
 function createRoverText(rover) {
   var wrapper = $("<div>");
   wrapper.addClass("rover-wrapper");
@@ -80,25 +23,97 @@ function createRoverText(rover) {
   var earthDate = rover.earth_date;
   var launchDate = rover.rover.launch_date;
   var landingDate = rover.rover.landing_date;
-
+​
   var roverNameEl = $("<li>");
   roverNameEl.text("Rover Name: " + roverName);
   roverInfoEl.append(roverNameEl);
-
+​
   var earthDateEl = $("<li>");
   earthDateEl.text("Earth Date: " + earthDate);
   roverInfoEl.append(earthDateEl);
-
+​
   var launchDateEl = $("<li>");
   launchDateEl.text("Launch Date: " + launchDate);
   roverInfoEl.append(launchDateEl);
-
+​
   var landingDateEl = $("<li>");
   landingDateEl.text("Landing Date: " + landingDate);
-
+​
   wrapper.append(roverNameEl);
   wrapper.append(earthDateEl);
   wrapper.append(launchDateEl);
   wrapper.append(landingDateEl);
   return wrapper;
 }
+​
+​
+$(document).ready(fetch(queryURL)
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  console.log(data);
+  let rover = data.latest_photos[1];
+  let landingEl;
+  if(rover){
+    $(carouselItem[0]).attr("src", rover.img_src);
+​
+    landingEl = createRoverText(rover);
+  }else{
+    rover = data.latest_photos[0];
+    if(rover){
+      $(carouselItem[0]).attr("src", rover.img_src);
+​
+      landingEl = createRoverText(rover);
+    }
+  }
+​
+​
+  roverInfoEl.append(landingEl);
+​
+  fetch(curQueryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      let landingEl2 = createRoverText(data.photos[0]);
+​
+      roverInfoEl.append(landingEl2);
+      landingEl2.attr("style", "display: none");
+​
+      $(carouselItem[1]).attr("src", data.photos[0].img_src);
+      $(".carousel-control-next").on("click", function () {
+​
+        if (state) {
+          $(actualItem[0]).removeClass("active");
+          $(actualItem[1]).addClass("active");
+          landingEl2.attr("style", "display: block");
+          landingEl.attr("style", "display: none");
+          state = !state;
+        } else {
+          state = !state;
+          $(actualItem[1]).removeClass("active");
+          $(actualItem[0]).addClass("active");
+          landingEl.attr("style", "display: block");
+          landingEl2.attr("style", "display: none");
+        }
+      });
+​
+      $(".carousel-control-prev").on("click", function () {
+        if (state) {
+          state = !state;
+          $(actualItem[0]).removeClass("active");
+          $(actualItem[1]).addClass("active");
+          landingEl2.attr("style", "display: block");
+          landingEl.attr("style", "display: none");
+        } else {
+          state = !state;
+          $(actualItem[1]).removeClass("active");
+          $(actualItem[0]).addClass("active");
+          landingEl.attr("style", "display: block");
+          landingEl2.attr("style", "display: none");
+        }
+      });
+    });
+}))
--- assets/JS/spaceevents.js
+++ assets/JS/spaceevents.js
@@ -1,16 +1,20 @@
 var spaceEventsContainer = $('#space-events');
 
 function getSpaceEvent() {
-    var spaceEventsURL = "https://lldev.thespacedevs.com/2.0.0/event/upcoming/";
+    var spaceEventsURL = "https://ll.thespacedevs.com/2.0.0/event/upcoming/";
 
-    fetch(spaceEventsURL)
+    fetch(spaceEventsURL, {
+        key: "Authorization",
+        value: "Token 30e8842577e5f6666308813bfefb7be8a7b38f40",
+    })
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
+            console.log(data);
             //dynamically generate elements to display
             var eventTitle = $('<h3>').addClass("card-title").text("Upcoming Events");
-            var eventName = $('<div>').addClass("card-header").text(data.results[0].name + moment(data.results[0].date).format("MMM, DD, YYYY"));
+            var eventName = $('<div>').addClass("card-header").text(data.results[0].name + " " + moment(data.results[0].date).format("MMM, DD, YYYY"));
             var eventDescription = $('<p>').addClass("card-text").text('"' + data.results[0].description + '"');
             var eventImage = $('<img>').addClass("card-img event-img").attr("src", data.results[0].feature_image);
             var eventURL = $('<a>').attr("href", data.results[0].news_url).attr("target", "_blank").text("Read more here");
